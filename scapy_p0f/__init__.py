baa52489c47f9564021c81ca45be647483761f4a
PavelPiatruk <erik.zbik@gmail.com>
2025-06-09 02:58:23 +0300

    px

--- scapy_p0f/__init__.py
+++ scapy_p0f/__init__.py
@@ -1,6 +1,6 @@
 from .p0fv3 import p0f, p0f_impersonate, prnp0f, fingerprint_mtu, p0fdb
 
-__version__ = "1.0.5"
+__version__ = "1.0.5e"
 
 __all__ = ["p0f", "p0f_impersonate", "prnp0f", "fingerprint_mtu", "p0fdb"]
 
